a07f44ad29d3aa7bef5a0d3b8a5ebde2a5fdb733
sieuvirut <smalldream.tvn@gmail.com>
2018-03-27 06:38:10 +0700

    Create folder.

---
+++ src/layout/bus92/menu/MenuTop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MenuTop extends Component {
+
+    render() {
+        return (
+            <div> MenuTop </div>
+        )
+    }
+
+}
+
+export default MenuTop
---
+++ src/layout/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import MenuTop from './bus92/menu/MenuTop'
+
+class Layout extends Component {
+
+	render() {
+		return (
+			<div>
+				<MenuTop />
+				<p> Trang chu  </p>
+			</div>
+		)
+	}
+}
+
+export default Layout
